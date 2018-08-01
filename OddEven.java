d385ab1298e0a1ce4051ead4d8c76b395b2f4d04
veenapk <kulveenap@gmail.com>
2018-08-01 12:38:24 +0530

    other pc

---
+++ OddEven.java
@@ -0,0 +1,18 @@
+class OddEven
+{
+	public static void main(String[] args)
+	{
+	int i;
+	for (i=30;i<=40;i++)
+	{
+	 	if(i%2==0)
+	  	{
+		System.out.println("Even Number "+i);
+		}
+		else
+		{
+		System.out.println("Odd Number "+i);
+		}
+	}
+	}
+}
