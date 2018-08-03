085109c6845b67e8c8ba65165640030c6030d1dc
veenapk <41325034+veenapk@users.noreply.github.com>
2018-08-03 15:05:01 +0530

    Update OddEven.java

--- OddEven.java
+++ OddEven.java
@@ -2,12 +2,13 @@ class OddEven
 {
 	public void hello()
 	{
+	System.out.println("hello");
 	}
 
 	public static void main(String[] args)
 	{
 	int i=0;
-	System.out.println("hello");
+	
 	for (i=30;i<=40;i++)
 	{
 	 	if(i%2==0)
